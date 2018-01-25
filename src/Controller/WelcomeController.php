d2224d57016080c02ddeff90184c5662c93a1b5b
Chris <chris@codereviewvideos.com>
2018-01-25 14:19:48 +0000

    vid-4

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class WelcomeController extends AbstractController
 {
@@ -13,5 +14,24 @@ class WelcomeController extends AbstractController
     public function index()
     {
         return $this->render('welcome/index.html.twig');
+    }
+
+    /**
+     * @Route(
+     *     "/hello-page/{name}",
+     *     name="hello_page",
+     *     defaults={"name" = "CodeReviewVideos.com"},
+     *     requirements={"name" = "[A-Za-z]+"}
+     * )
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function hello($name)
+    {
+        return $this->render(
+            'hello_page.html.twig',
+            [
+                'name' => $name
+            ]
+        );
     }
 }
