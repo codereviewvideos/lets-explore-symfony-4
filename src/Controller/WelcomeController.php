bdccebf1709ae08e55ec6493acfdb4e305fbe3b4
Chris <chris@codereviewvideos.com>
2018-01-25 13:22:21 +0000

    vid-2

---
+++ src/Controller/WelcomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class WelcomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="welcome")
+     */
+    public function index()
+    {
+        return $this->render('welcome/index.html.twig');
+    }
+}
